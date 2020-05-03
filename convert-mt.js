3e2bb3758443023325e8a0cb9d1026417342a21b
Takahiko Wada <wada.takahiko@gmail.com>
2020-05-03 15:05:10 +0900

    read exttended body

--- convert-mt.js
+++ convert-mt.js
@@ -1,18 +1,18 @@
-const turndown = require("turndown");
-const fs = require("fs");
-const moment = require("moment");
+const turndown = require('turndown');
+const fs = require('fs');
+const moment = require('moment');
 
 const FILE_PATH = process.argv[2];
 if (!FILE_PATH) {
-  console.log("[Error] enter file path like... node convert-mt.js input.txt");
+  console.log('[Error] enter file path like... node convert-mt.js input.txt');
   return;
 }
 
-const OUTPUT_PATH = "./output";
+const OUTPUT_PATH = './output';
 
 const htmlToMarkdown = (html) => {
   const turndownService = new turndown({
-    headingStyle: "atx",
+    headingStyle: 'atx',
   });
   const markdown = turndownService.turndown(html);
   //console.log(markdown);
@@ -20,34 +20,51 @@ const htmlToMarkdown = (html) => {
 };
 
 const readFile = (file) => {
-  const text = fs.readFileSync(file, "utf-8");
-  const res = text.split("--------");
+  const text = fs.readFileSync(file, 'utf-8');
+  const res = text.split('--------');
   //return res.splice(0, 1);
   return res;
 };
 
 const extractLabel = (content, label) => {
-  const matches = content.match(label + ": (.*)\n");
+  const matches = content.match(label + ': (.*)\n');
   if (!matches) return null;
   return matches[1];
 };
 
+const extractLines = (content, label) => {
+  const lines = content.split('\n');
+  let isStart = false;
+  let result = '';
+  for (const line of lines) {
+    if (isStart && line === '-----') {
+      break;
+    }
+    if (isStart) {
+      result = result + line + '<br>\n';
+    }
+    if (line === `${label}:`) {
+      isStart = true;
+    }
+  }
+  return result;
+};
+
 const extractBody = (content) => {
-  const matches = content.split("-----");
-  if (!matches || matches.length < 2) return null;
-  return matches[1].replace("BODY:\n", "");
+  const body = extractLines(content, 'BODY');
+  const extentedBody = extractLines(content, 'EXTENDED BODY');
+  return body + extentedBody;
 };
 
 const parseContent = (content) => {
-  const title = extractLabel(content, "TITLE");
-  const date = extractLabel(content, "DATE");
-  const category = extractLabel(content, "CATEGORY");
-  const image = extractLabel(content, "IMAGE");
-  const status = extractLabel(content, "STATUS");
-  //console.log(title, date, category, image);
+  const title = extractLabel(content, 'TITLE');
+  const date = extractLabel(content, 'DATE');
+  const category = extractLabel(content, 'CATEGORY');
+  const image = extractLabel(content, 'IMAGE');
+  const status = extractLabel(content, 'STATUS');
   const bodyHtml = extractBody(content);
-  const bodyMarkdown = bodyHtml ? htmlToMarkdown(bodyHtml) : "";
-  //console.log(bodyMarkdown);
+  const bodyMarkdown = bodyHtml ? htmlToMarkdown(bodyHtml) : '';
+  console.log(bodyMarkdown);
   return {
     title,
     date,
@@ -55,7 +72,7 @@ const parseContent = (content) => {
     image,
     status,
     bodyMarkdown,
-    moment: moment(date, "MM/DD/YYYY hh:mm:ss"),
+    moment: moment(date, 'MM/DD/YYYY hh:mm:ss'),
   };
 };
 
@@ -71,11 +88,11 @@ ${parseResult.bodyMarkdown}`;
 const outputGatsbyFile = (parseResult) => {
   const gatsbyMarkdown = getGatsbyMarkdown(parseResult);
   // create directory
-  const year = parseResult.moment.format("YYYY");
-  const month = parseResult.moment.format("MM");
-  const day = parseResult.moment.format("DD");
+  const year = parseResult.moment.format('YYYY');
+  const month = parseResult.moment.format('MM');
+  const day = parseResult.moment.format('DD');
   const dirPath = `${OUTPUT_PATH}/${year}${month}${day}`;
-  fs.mkdirSync(dirPath, { recursive: true }, (err) => {
+  fs.mkdirSync(dirPath, {recursive: true}, (err) => {
     if (err) throw err;
   });
   // write file
@@ -86,7 +103,7 @@ const main = () => {
   const contents = readFile(FILE_PATH);
   contents.forEach((content) => {
     const parseResult = parseContent(content);
-    if (parseResult.status === "Publish") {
+    if (parseResult.status === 'Publish') {
       outputGatsbyFile(parseResult);
       console.log(parseResult.date, parseResult.title);
     }
